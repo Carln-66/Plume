b4a5844705140ed9bcf3300f978dc0d9e8f4d82e
Carl <carln@foxmail.com>
2021-04-12 13:08:56 +0100

    v10-添加AOP切面实现接口日志记录

---
+++ plume10/src/main/java/com/plume/mall/component/RestAuthenticationEntryPoint.java
@@ -0,0 +1,30 @@
+package com.plume.mall.component;
+
+import cn.hutool.json.JSONUtil;
+import com.plume.mall.common.api.CommonResult;
+import org.springframework.security.core.AuthenticationException;
+import org.springframework.security.web.AuthenticationEntryPoint;
+import org.springframework.stereotype.Component;
+
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+
+/**
+ * @Auther: Carl
+ * @Date: 2021/04/09/15:33
+ * @Description: 当未登录或者token失效访问接口时自定义返回结果
+ */
+@Component
+public class RestAuthenticationEntryPoint implements AuthenticationEntryPoint {
+    @Override
+    public void commence(HttpServletRequest httpServletRequest,
+                         HttpServletResponse httpServletResponse,
+                         AuthenticationException e) throws IOException, ServletException {
+        httpServletResponse.setCharacterEncoding("UTF-8");
+        httpServletResponse.setContentType("application/json");
+        httpServletResponse.getWriter().println(JSONUtil.parse(CommonResult.unauthorized(e.getMessage())));
+        httpServletResponse.getWriter().flush();
+    }
+}
